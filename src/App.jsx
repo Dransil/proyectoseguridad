67d9eedd8234cd19d432595332399939496dcb42
Emanuel Taquichiri <thecobra1783@gmail.com>
2023-08-19 00:08:35 -0400

    AppRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { AppRoute } from './routes/appRoute';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-    <>
-      
-    </>
+    <AppRoute/>
   )
 }
-
 export default App
