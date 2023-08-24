fb5ad426a997e865a86c18169d8f6b2449b5a587
Emanuel Taquichiri <thecobra1783@gmail.com>
2023-08-24 18:14:18 -0400

    navbar

--- src/components/navigation/menu.jsx
+++ src/components/navigation/menu.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Button from '@mui/material/Button'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 
 export const Menu = () => {
     return (
@@ -9,6 +13,13 @@ export const Menu = () => {
                 <meta name="description" content="" />
                 <meta name="author" content="" />
                 <title>SecureServ</title>
+                <AppBar position="fixed" color="secondary">
+                  <Toolbar>
+                    <Typography variant="h6">
+                      
+                    </Typography>
+                  </Toolbar>
+                </AppBar>
             </header>
             <body>
                 
