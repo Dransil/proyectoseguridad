c5b64959e4f51bebe085641984937c2747bbf72b
Emanuel Taquichiri <thecobra1783@gmail.com>
2023-08-24 18:03:47 -0400

    header

--- src/components/navigation/menu.jsx
+++ src/components/navigation/menu.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Menu = () => {
     return (
         <>
-        <h1>
+        <header>
             Iniciando menu
-        </h1>
+        </header>
         </>
     )
 }
