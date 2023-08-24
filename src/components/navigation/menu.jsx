f38d1d624a789317560ce5e036be825a1602055a
Emanuel Taquichiri <thecobra1783@gmail.com>
2023-08-24 18:11:47 -0400

    add mui

--- src/components/navigation/menu.jsx
+++ src/components/navigation/menu.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 export const Menu = () => {
     return (
-        <>
-        <header>
-            Iniciando menu
-        </header>
-        </>
+        <html>
+            <header>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="description" content="" />
+                <meta name="author" content="" />
+                <title>SecureServ</title>
+            </header>
+            <body>
+                
+            </body>
+        </html>
     )
 }
